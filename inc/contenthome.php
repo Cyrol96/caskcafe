10ef3a7cec4e201ad16e2ba44cf7b5e5ba274cfc
cyrol96 <abdulazeezj@assiniboine.net>
2023-09-27 22:21:45 -0600

    1

--- inc/contenthome.php
+++ inc/contenthome.php
@@ -22,25 +22,24 @@ if (!$result) {
 }
 ?>
 
-
 <!-- </article> -->
 <div id="header">
     <h1>Cask Cafe</h1>
 </div>
 <!-- The video -->
 <video autoplay muted loop id="myVideo">
-  <source src="video\parfait_video.mp4" type="video/mp4">
+    <source src="video\parfait_video.mp4" type="video/mp4">
 </video>
 
 <!-- some overlay text to describe the video -->
 <div class="content">
-<h2 class="home-intro text-dark">Our Fruit Pairfait</h2>
-<h5 class="text-black">We are open for today with a happy cup of parfait from Cask Cafe 🍧🥮 !
-    We are open and ready to delight you with our fresh and yummy parfait!
-    Please give us a try and you'll keep coming back for more ☺️</h5>
-  <!-- Use a button to pause/play the video with JavaScript -->
-  <!-- <button id="myBtn" onclick="myFunction()">Pause</button> -->
-    </div>
+    <h2 class="home-intro text-dark">Our Fruit Pairfait</h2>
+    <h5 class="text-black">We are open for today with a happy cup of parfait from Cask Cafe 🍧🥮 !
+        We are open and ready to delight you with our fresh and yummy parfait!
+        Please give us a try and you'll keep coming back for more ☺️</h5>
+    <!-- Use a button to pause/play the video with JavaScript -->
+    <!-- <button id="myBtn" onclick="myFunction()">Pause</button> -->
+</div>
 
 <!-- Add drop-down list for filtering by category -->
 <form method="get ">
@@ -62,26 +61,29 @@ if (!$result) {
             if (mysqli_num_rows($result) > 0):
                 while ($row = mysqli_fetch_assoc($result)):
                     ?>
-                    <div class="col-md-4 gallery">
-                        <img src="<?php echo $row['prod_img']; ?>" alt="<?php echo $row['prod_name']; ?>" width="300" height="300">
-                        <div class="desc bg-secondary text-light">
-                            <?php echo $row['prod_name']; ?> <br>$
-                            <?php echo $row['Price']; ?>
-                        </div>
-                        
-                        <!-- Add to Cart Form -->
-                        <div class="w3-container w3-card-4 w3-light-grey w3-text-dark-grey w3-margin-top" style="max-width: 400px;">
-                            <form action="addcart.php" method="post" class="w3-container">
-                                <input type="hidden" name="product_id" value="1">
-                                <input type="hidden" name="product_name" value="Example Product">
-                                <input type="hidden" name="product_price" value="19.99">
-                                <label for="quantity" class="w3-text-dark-grey"><b>Quantity:</b></label>
-                                <input type="number" id="quantity" name="quantity" value="1" min="1" class="w3-input w3-border">
-                                <button type="submit" class="w3-button w3-block w3-blue w3-section w3-padding">Add to Cart</button>
-                            </form>
-                        </div>
-                    </div>
-                <?php endwhile;
+         <div class="col-md-4 gallery">
+             <img src="<?php echo $row['prod_img']; ?>" alt="<?php echo $row['prod_name']; ?>" width="300"
+           height="300">
+        <div class="desc bg-secondary text-light">
+        <?php echo $row['prod_name']; ?> <br>$
+          <?php echo $row['Price']; ?>
+     </div>
+
+     <!-- Add to Cart Form -->
+     <div class="w3-container w3-card-4 w3-light-grey w3-text-dark-grey w3-margin-top"
+        style="max-width: 400px;">
+        <form action="addcart.php" method="post" class="w3-container">
+        <input type="hidden" name="product_id" value="1">
+        <input type="hidden" name="product_name" value="Example Product">
+         <input type="hidden" name="product_price" value="19.99">
+         <label for="quantity" class="w3-text-dark-grey"><b>Quantity:</b></label>
+     <input type="number" id="quantity" name="quantity" value="1" min="1" class="w3-input w3-border">
+     <button type="submit" class="w3-button w3-block w3-blue w3-section w3-padding">Add to Cart</button>
+     </form>
+     </div>
+        </div>
+        
+        <?php endwhile;
             else: ?>
                 <div class="col-md-12 text-center">
                     <h1>No product Available</h1>
