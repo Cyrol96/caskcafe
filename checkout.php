dea05d424239ccf0d3c6183ff0ee8115d0c50686
cyrol96 <abdulazeezj@assiniboine.net>
2023-10-08 01:21:35 -0600

    1

--- checkout.php
+++ checkout.php
@@ -1,87 +1,122 @@
 <?php
-// session_start();
-$page_title = "Checkout";
-$self = basename($_SERVER['PHP_SELF']);
-include 'inc/header.php';
+// Session start
+session_start();
 
-// Fetch all necessary product information from the database for the cart items
-$productData = array(); // An array to store product information
-if ($_SESSION['cart']) {
-    require('./dbconnect/dbconnect.php');
-    $sql = "SELECT prod_id, prod_name, price, prod_img FROM pro_info WHERE prod_id IN (";
-    foreach ($_SESSION['cart'] as $prod_id => $item) {
-        $sql .= $prod_id . ',';
-    }
-    $sql = substr($sql, 0, -1) . ') ORDER BY prod_name ASC';
-    $result = mysqli_query($dbc, $sql);
+// Include required functions
+require('inc/loginfunctions.php');
 
-    // Store product information in the $productData array
-    while ($row = mysqli_fetch_assoc($result)) {
-        $productData[$row['prod_id']] = array(
-            'prod_name' => $row['prod_name'],
-            'price' => $row['price']
-        );
-    }
-}
-
-echo '<div class="container mt-5">';
-if (empty($_SESSION['cart'])) {
-    // Display a message if the cart is empty
-    echo '<div class="alert alert-info"><h2>Your Cart is Empty</h2></div>';
+// Redirect if no session variables exist
+if (!isset($_SESSION['userid'])) {
+    redirect_user();
+} elseif (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
+    redirect_user('viewcart.php');
 } else {
-    echo '<table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Subtotal</th>
-                </tr>
-            </thead>
-            <tbody>';
+    // All good, continue processing
 
-    $total = 0;
+    // Change title and include header
+    $self = basename($_SERVER['PHP_SELF']);
+    $page_title = 'Checkout | By Caskcafe';
+    include('inc/header.php');
 
-    foreach ($_SESSION['cart'] as $prod_id => $item) {
-        $quantity = $item['quantity'];
-        $price = $productData[$prod_id]['price'];
-        $product_name = $productData[$prod_id]['prod_name'];
-        $subtotal = $quantity * $price;
-        $total += $subtotal;
+    echo "<div class='w3-container w3-teal'><h1>" . $page_title . "</h1></div>";
 
-        // Display cart items
-        echo '<tr>
-                <td>' . $product_name . '</td>
-                <td>$' . number_format($price, 2) . '</td>
-                <td>' . $quantity . '</td>
-                <td>$' . number_format($subtotal, 2) . '</td>
-              </tr>';
-    }
+    // Connect to the database
+    require('./dbconnect/dbconnect.php');
 
-    // Display the total
-    echo '<tr>
-            <td colspan="3"><strong>Total:</strong></td>
-            <td>$' . number_format($total, 2) . '</td>
-          </tr>';
-    echo '</tbody></table>';
+    // Set user ID and total into variables
+    $userid = $_SESSION['userid'];
+    $total = $_SESSION['total'];
 
-    // Add a "Place Order" button
-    echo '<div class="text-center">
-            <form method="post" action="place_order.php">
-                <button type="submit" class="btn btn-primary">Place Order</button>
-            </form>
-          </div>';
+    // Turn off autocommit
+    mysqli_autocommit($dbc, FALSE);
+
+    // Insert the order header into the orders table
+    // Check if 'total' is set and not empty
+if (isset($_SESSION['total']) && $_SESSION['total'] !== '') {
+    $total = $_SESSION['total'];
+} else {
+    // Handle the case where 'total' is not set or empty
+    // You can display an error message or redirect the user
+    echo "Total is not set or empty. Please go back and add items to your cart.";
+    exit; // Exit the script to prevent further execution
 }
 
-// Add a button to continue shopping
-echo '<p class="indent mt-3">
-        <a href="index.php">
-            <button class="btn btn-secondary">Continue Shopping</button>
-        </a>
-      </p>
-    </div>
-</div>';
+// Now you can proceed with the insertion
+$sql = "INSERT INTO order_info (customer_id, total) VALUES (?, ?)";
+$stmt = mysqli_prepare($dbc, $sql);
+mysqli_stmt_bind_param($stmt, 'id', $userid, $total);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
 
-// Include the footer
-include("./inc/footer.php");
+
+    // Check if the insertion was successful
+    if (mysqli_affected_rows($dbc) == 1) {
+        // Retrieve the newly created order ID
+        $orderid = mysqli_insert_id($dbc);
+
+        // Prepared statement for inserting order items
+        $insert = "INSERT INTO order_info (order_id, product_id, quantity) VALUES (?, ?, ?)";
+        $stmt = mysqli_prepare($dbc, $insert);
+        mysqli_stmt_bind_param($stmt, 'iid', $orderid, $prod_id, $qty);
+
+        // Run the query loop to insert order items
+        $affected = 0;
+        foreach ($_SESSION['cart'] as $prod_id => $item) {
+            $qty = $item['quantity'];
+            $prod_id = $item['prod_id']; // Assuming you have the product ID in the cart
+            mysqli_stmt_execute($stmt);
+            $affected += mysqli_stmt_affected_rows($stmt);
+        }
+
+        // Close the prepared statement
+        mysqli_stmt_close($stmt);
+
+        // Check if all items were inserted successfully
+        if ($affected == count($_SESSION['cart'])) {
+            // Commit the transaction
+            mysqli_commit($dbc);
+
+            // Unset the cart and total
+            unset($_SESSION['cart']);
+            unset($_SESSION['total']);
+
+            // Get the order timestamp from the orders table (replace with the correct table name)
+            $q = "SELECT order_date FROM order_info WHERE order_id = $orderid LIMIT 1";
+            $r = mysqli_query($dbc, $q);
+
+            if ($r) {
+                $row = mysqli_fetch_assoc($r);
+
+                // Format the date and time
+                $order_date = date("l, F d, Y", strtotime($row['order_date']));
+                $order_time = date("g:i:s A", strtotime($row['order_date']));
+
+                // Display a success message
+                echo "<div class='w3-container w3-camo-sandstone'><h2>Your order has been placed!</h2>";
+                echo "<p class='indent'>Your order number is <span class='w3-camo-red'><i>" . date("Y") . "-$orderid</i></span> placed at $order_time on $order_date. The total of your order was <span class='w3-camo-red'><i>$" . number_format($total, 2) . "</i></span>. Thank you for your purchase!</p><div class='logout'><p class='indent'><a href='logout.php'><button>Logout</button></a></p></div></div><img src='./img/site/success.jpg' id='successimg'>";
+            } else {
+                // Error handling for timestamp retrieval
+                echo "<div class='w3-container w3-red'><p class='indent'>Error retrieving order timestamp.</p></div>";
+            }
+        } else {
+            // Rollback the transaction if order items were not inserted successfully
+            mysqli_rollback($dbc);
+
+            // Error message
+            echo "<div class='w3-container w3-red'><p class='indent'>There was a problem completing your order. Please contact customer support for assistance. No payment has been charged at this point.</p></div>";
+        }
+    } else {
+        // Rollback the transaction if the order header was not inserted successfully
+        mysqli_rollback($dbc);
+
+        // Error message
+        echo "<div class='w3-container w3-red'><p class='indent'>There was a problem completing your order. Please contact customer support for assistance. No payment has been charged at this point.</p></div>";
+    }
+
+    // Close the database connection
+    mysqli_close($dbc);
+
+    // Include footer
+    include('inc/footer.php');
+}
 ?>
--- loggedin.php
+++ loggedin.php
@@ -1,7 +1,14 @@
 <?php
-//session_start();
+session_start(); // Start the session
 
 $cookie_name = "firstname";
+
+// Check for login
+if (!isset($_SESSION['userid']) && !isset($_SESSION['agent'])) {
+    // Redirect to the login page or another appropriate location
+    header("Location: login.php");
+    exit();
+}
 
 // Change title and include header
 $self = basename($_SERVER['PHP_SELF']);
@@ -11,12 +18,10 @@ include('inc/header.php');
 // Display a customized successful login message
 var_dump($_SESSION);
 echo "<div class='w3-container w3-teal'><h1>" . $page_title . "
-<a href='viewcart.php'><i class='fas fa-shopping-cart' id='caticon' 
-title='View Cart'></i></a></h1></div> 
-<div class='container'><p class='indent'>You are now logged in, 
-</p>
-<div class='logout'><p><a href='index.php'><button id='updateuser'>
-Let's go shopping</button></a><a href='logout.php' ><button>LOGOUT</button></a></p></div></div>";
+<a href='viewcart.php'><i class='fas fa-shopping-cart' id='caticon' title='View Cart'></i></a></h1></div> 
+<div class='container'><p class='indent'>You are now logged in.</p>
+<div class='logout'><p><a href='index.php'><button id='updateuser'>Let's go shopping</button></a>
+<a href='logout.php'><button>LOGOUT</button></a></p></div></div>";
 
 // Footer
 require('inc/footer.php');
