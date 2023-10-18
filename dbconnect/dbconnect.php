74b3e71822e9c0622a1bda2b9089dcee591663ec
cyrol96 <abdulazeezj@assiniboine.net>
2023-10-18 09:16:14 -0600

    Jay

--- dbconnect/dbconnect.php
+++ dbconnect/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 // Define the database connection constants
 define('DB_HOST', 'localhost');
-define('DB_USER', 'jamaldeen');
-define('DB_PASSWORD', 'Kingpin@26');
-define('DB_NAME', 'Caskcafe_2');
+define('DB_USER', 'ccm_jamaldeen');
+define('DB_PASSWORD', 'k7aq@WLnCLrG');
+define('DB_NAME', 'ccm_jamaldeen');
 
 // Establish a connection to the database
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
